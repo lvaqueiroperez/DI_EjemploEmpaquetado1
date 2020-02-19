9470cd29fe029c8cb09d30e1b530ec84649747b0
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2020-02-19 11:36:45 +0100

    "ejemplo empaquetado 1"

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-descripcion_larga = open
+descripcion_larga = open('Readme.rst').read()
 
 setup(
     name = "Exemplo de empaquetado",
     version = "0.12",
     description = "Descripción breve del Ejemplo de empaquetado",
     long_description = descripcion_larga,
+    keywords = "empaquetado instalador paquetes",
+    packages = []
 )
